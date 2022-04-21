8352e2daa83f8854deb4c70722a9ad54cc8b0690
Nurlanbek <60207085+nurlanek@users.noreply.github.com>
2022-04-21 20:21:15 +0600

    dd
    dd

--- results.php
+++ results.php
@@ -1,17 +1,42 @@
 <?php
-session_start();
+//session_start();
+
+require_once 'header.php';
 require_once 'dbConn.php';
-$userId = getUserId();
-echo "I am results.php<br>";
-function getUserId(){
-    $userId =  pdo_connect_mysql()->prepare("select id from users where phone_number = ?");
-    $userId->execute([$_SESSION['phone_number']]);
-    $userId =$userId->fetchAll();
-    return $userId[0]['id'];
-}
+// $userId = getUserId();
+// echo "I am results.php<br>";
+// function getUserId(){
+//     $userId =  pdo_connect_mysql()->prepare("select id from users where phone_number = ?");
+//     $userId->execute([$_SESSION['phone_number']]);
+//     $userId =$userId->fetchAll();
+//     return $userId[0]['id'];
+// }
 
 
-echo $userId;
 
-//when you're done with sessions, destroy it
+ //echo $userId;
+ $userId = 86;
+
+$pdo = pdo_connect_mysql();
+
+$sql = "SELECT COUNT(answer_value) FROM answers where vertical_id  = 1 and answer_value = 1 and user_id = $userId";
+$res = $pdo->query($sql);
+$count_a1 = $res->fetchColumn();
+
+$sql = "SELECT COUNT(answer_value) FROM answers where horizontal_id= 1 and answer_value = 2 and user_id = $userId";
+$res = $pdo->query($sql);
+$count_b1 = $res->fetchColumn();
+$resul1 = $count_a1+$count_b1
+
+?>
+
+Result-1 = <?php echo "$resul1<br>"?> 
+<!-- Result-2 = <?php echo "$resul2<br>"?> 
+Result-3 = <?php echo "$resul3<br>"?> 
+Result-4 = <?php echo "$resul4<br>"?>  -->
+
+
+<?php
+// //when you're done with sessions, destroy it
 //session_destroy();
+?>
